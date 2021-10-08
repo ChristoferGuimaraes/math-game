bbe84a2e6ff25f1a6fd4c80c9ae5d4062c1e8cac
Christofer <christoferguima@gmail.comnam>
2021-10-08 10:33:19 -0300

    game is now functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  /* const [num1, setNum1] = useState(1);
+  const [num2, setNum2] = useState(2); */
+  const [state, setState] = useState({
+    num1: 5,
+    num2: 8,
+    response: "",
+    score: 0,
+    onScreen: ''
+  });
+
+  function inputKeyPress(event) {
+    if (event.key === "Enter") {
+      const answer = parseInt(state.response);
+      if (state.num1 + state.num2 === answer) {
+        setState({
+          num1: Math.ceil(Math.random() * 10),
+          num2: Math.ceil(Math.random() * 10),
+          score: state.score + 1,
+          response: '',
+          onScreen: "Correct!"
+        });
+      } else {
+        setState({
+          ...state,
+          score: state.score - 1,
+          response: '',
+          onScreen: "Wrong!"
+        });
+      }
+    }
+  }
+
+  function updateResponse(event) {
+    setState({
+      ...state,
+      response: event.target.value,
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{state.onScreen}</div>
+      <div>
+        {state.num1}+{state.num2}
+      </div>
+      <input
+        onKeyPress={inputKeyPress}
+        onChange={updateResponse}
+        value={state.response}
+      />
+      <div>Score: {state.score}</div>
+    </>
   );
 }
 
